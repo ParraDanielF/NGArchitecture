af658a80477c1c9a2f9a71d18315842cd8e8433e
parradafe <daniel.parra@globant.com>
2021-05-23 00:30:58 -0500

    was cleaned the branch to support only the required files to implement the bl

--- clinicHistory(Query)/src/resolver.js
+++
@@ -1,9 +0,0 @@
-import {data} from './persistence/mock/data.json';
-
-export const resolver = {
-    Query:{
-        getHistoryData(root, {userID}){
-            return data.filter(d => d.userId === userID);
-        }
-    }
-};
